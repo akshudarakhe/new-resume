8d242cf514d4135a42067e0a8ba225d1999c0bfb
akshudarakhe <132133824+akshudarakhe@users.noreply.github.com>
2023-05-02 21:53:04 +0530

    yes

---
+++ src/app/education/education.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  templateUrl: './education.component.html',
+  styleUrls: ['./education.component.css']
+})
+export class EducationComponent {
+
+  degree = 'BCS';
+  university = 'Pune University';
+  year = '2022';
+}
